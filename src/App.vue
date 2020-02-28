e020b822fbb7cfe26ebb326af6327a6c7a1353fe
obinnadtq <oisiwekpeni@gmail.com>
2020-02-28 18:34:19 +0100

    created the structure for the app

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <quiz-body></quiz-body>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import QuizBody from './components/QuizBody.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    QuizBody
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QuizBody.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="quiz-body">
+      <h3>Some Questions Will Go in here</h3>
+      <hr>
+      <p> List of Answers come here </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.quiz-body{
+    height: 500px;
+    width: 50%;
+    background-color: lightgray;
+    margin: auto;
+}
+
+h3 {
+    padding: 30px 0 30px 0;
+}
+</style>
